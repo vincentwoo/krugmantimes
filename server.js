7c80e4f61cd4d18b85078b1629ca161bb5ad33ea
Vincent Woo <me@vincentwoo.com>
2013-02-02 01:11:30 -0800

    Basic Krugmanship

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 var express = require('express');
-var redis = require('redis');
-var url = require('url');
+var redis   = require('redis');
+var url     = require('url');
 var cheerio = require('cheerio');
 var request = require('request');
 
+var KRUGMANZ = [
+  'http://media.salon.com/2010/04/pistols_at_dawn_paul_krugman_vs_andrew_sorkin.jpg',
+  'http://www.wired.com/images_blogs/underwire/2012/05/Krugman-6601.jpg',
+  'http://www.skepticmoney.com/wp-content/uploads/2012/08/Paul-Krugman.jpg',
+  'http://www.princeton.edu/~paw/web_exclusives/more/more_pics/more6_krugman.jpg'
+];
 var db;
 var app = express();
 
@@ -18,7 +24,16 @@ app.configure('production', function() {
 
 app.get('/', function(req, res){
   request('http://www.nytimes.com', function (error, response, body) {
-    if (error || response.statusCode != 200) return;
+    $ = cheerio.load(body);
+    $('.byline').text('By PAUL KRUGMAN');
+    $('img').each(function(idx, element) {
+      element = $(element);
+      if (element.attr('id') == 'mastHead') return;
+      if (!element.attr('width') || !element.attr('height')) return;
+      $(this).attr('src', KRUGMANZ[idx % KRUGMANZ.length]);
+    });
+
+    body = $.html();
     res.setHeader('Content-Type', 'text/html');
     res.setHeader('Content-Length', body.length);
     res.end(body);
