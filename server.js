5dab4a692f5c184d0d5f6f205f6df4343465b588
Vincent Woo <me@vincentwoo.com>
2013-02-02 04:34:22 -0800

    Add the krugman times logo

--- server.js
+++ server.js
@@ -11,24 +11,37 @@ var KRUGMANZ = [
   'http://www.princeton.edu/~paw/web_exclusives/more/more_pics/more6_krugman.jpg'
 ];
 var db;
+var maxAge;
 var app = express();
 
 app.configure('development', function() {
   db = redis.createClient();
+  maxAge = 0;
 });
 app.configure('production', function() {
   var redisURL = url.parse(process.env.REDISCLOUD_URL);
   db = redis.createClient(
     redisURL.port, redisURL.hostname, {no_ready_check: true});
+  maxAge = 86400000;
 });
 
 app.get('/', function(req, res) {
   request('http://www.nytimes.com', function (error, response, body) {
     $ = cheerio.load(body);
+
+    $('title').text('The Krugman Times');
     $('.byline').text('By PAUL KRUGMAN');
+
     $('img').each(function(idx, element) {
       element = $(element);
-      if (element.attr('id') == 'mastHead') return;
+
+      if (element.attr('id') == 'mastheadLogo') {
+        element.attr('src', '/images/krugman_times_logo.png');
+        element.parent().prev().remove(); // Delete document.write script tag
+        element.parent().replaceWith(element); // replace noscript tag with image
+        return;
+      }
+
       if (!element.attr('width') || !element.attr('height')) return;
       element.attr('src', KRUGMANZ[idx % KRUGMANZ.length]);
     });
@@ -40,5 +53,7 @@ app.get('/', function(req, res) {
     res.end(body);
   });
 });
+
+app.use(express.static(__dirname + '/public', {maxAge: maxAge}));
 
 app.listen(process.env.PORT || 5000);
