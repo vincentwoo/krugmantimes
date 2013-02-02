b196a8604ae56874514b2337555549de620fc741
Vincent Woo <me@vincentwoo.com>
2013-02-02 00:34:57 -0800

    Fix redis connection

--- server.js
+++ server.js
@@ -1,14 +1,24 @@
 var express = require('express');
 var redis = require('redis');
+var url = require('url');
 var cheerio = require('cheerio');
 var request = require('request');
 
-var db = redis.createClient();
+var db;
 var app = express();
+
+app.configure('development', function() {
+  db = redis.createClient();
+});
+app.configure('production', function() {
+  var redisURL = url.parse(process.env.REDISCLOUD_URL);
+  db = redis.createClient(
+    redisURL.port, redisURL.hostname, {no_ready_check: true});
+});
 
 app.get('/', function(req, res){
   request('http://www.nytimes.com', function (error, response, body) {
-  if (error || response.statusCode != 200) return;
+    if (error || response.statusCode != 200) return;
     res.setHeader('Content-Type', 'text/html');
     res.setHeader('Content-Length', body.length);
     res.end(body);
