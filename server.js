500bac1b53f10292bf6027e06a1dbd2facbb43f4
Vincent Woo <me@vincentwoo.com>
2013-02-02 00:24:01 -0800

    Initial simple passthrough

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 var express = require('express');
+var redis = require('redis');
+var cheerio = require('cheerio');
+var request = require('request');
+
+var db = redis.createClient();
 var app = express();
 
 app.get('/', function(req, res){
-  var body = "Hello Krugman's World";
-  res.setHeader('Content-Type', 'text/plain');
-  res.setHeader('Content-Length', body.length);
-  res.end(body);
+  request('http://www.nytimes.com', function (error, response, body) {
+  if (error || response.statusCode != 200) return;
+    res.setHeader('Content-Type', 'text/html');
+    res.setHeader('Content-Length', body.length);
+    res.end(body);
+  })
 });
 
 app.listen(process.env.PORT || 5000);
